9bda9702927e4a9a3441faa4b28f32d613216761
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-08-17 22:37:46 +0200

    feat: TODO and hints

--- main.cpp
+++ main.cpp
@@ -1,22 +1,19 @@
 
-/**
- * @brief - Canonical application allowing to instantiate
- *          a working PGE process with configurable hooks
- *          to customize the behavior.
- */
+/// @brief - A sudoku solver.
 
-# include <core_utils/StdLogger.hh>
-# include <core_utils/PrefixedLogger.hh>
-# include <core_utils/LoggerLocator.hh>
-# include <core_utils/CoreException.hh>
-# include "AppDesc.hh"
-# include "TopViewFrame.hh"
-# include "App.hh"
+#include "App.hh"
+#include "AppDesc.hh"
+#include "TopViewFrame.hh"
+#include <core_utils/CoreException.hh>
+#include <core_utils/LoggerLocator.hh>
+#include <core_utils/PrefixedLogger.hh>
+#include <core_utils/StdLogger.hh>
 
 /// TODO: Hint in main game.
+/// https://github.com/cyrixmorten/sudoku/tree/master/src/solver/solverStrategies
+/// https://www.mpl.live/blog/sudoku-hints-to-solve-sudoku-puzzles-logically/
 
-int
-main(int /*argc*/, char** /*argv*/) {
+int main(int /*argc*/, char ** /*argv*/) {
   // Create the logger.
   utils::StdLogger raw;
   raw.setLevel(utils::Level::Debug);
@@ -26,28 +23,29 @@ main(int /*argc*/, char** /*argv*/) {
   try {
     logger.logMessage(utils::Level::Notice, "Starting application");
 
-    pge::Viewport tViewport = pge::Viewport(olc::vf2d(-1.0f, -1.0f), olc::vf2d(11.0f, 11.0f));
-    pge::Viewport pViewport = pge::Viewport(olc::vf2d(0.0f, 0.0f), olc::vf2d(768.0f, 768.0f));
+    pge::Viewport tViewport =
+        pge::Viewport(olc::vf2d(-1.0f, -1.0f), olc::vf2d(11.0f, 11.0f));
+    pge::Viewport pViewport =
+        pge::Viewport(olc::vf2d(0.0f, 0.0f), olc::vf2d(768.0f, 768.0f));
 
     pge::CoordinateFrameShPtr cf = std::make_shared<pge::TopViewFrame>(
-      tViewport,
-      pViewport,
-      olc::vi2d(64, 64)
-    );
+        tViewport, pViewport, olc::vi2d(64, 64));
     pge::AppDesc ad = pge::newDesc(olc::vi2d(768, 768), cf, "sudoku");
     ad.fixedFrame = true;
     pge::App demo(ad);
 
     demo.Start();
-  }
-  catch (const utils::CoreException& e) {
-    logger.logError(utils::Level::Critical, "Caught internal exception while setting up application", e.what());
-  }
-  catch (const std::exception& e) {
-    logger.logError(utils::Level::Critical, "Caught internal exception while setting up application", e.what());
-  }
-  catch (...) {
-    logger.logMessage(utils::Level::Critical, "Unexpected error while setting up application");
+  } catch (const utils::CoreException &e) {
+    logger.logError(utils::Level::Critical,
+                    "Caught internal exception while setting up application",
+                    e.what());
+  } catch (const std::exception &e) {
+    logger.logError(utils::Level::Critical,
+                    "Caught internal exception while setting up application",
+                    e.what());
+  } catch (...) {
+    logger.logMessage(utils::Level::Critical,
+                      "Unexpected error while setting up application");
   }
 
   return EXIT_SUCCESS;
